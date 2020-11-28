2e92f36c2c1ca5b661cc81c3c2a57bbacb5d6316
Alex <ati12611@mail.ru>
2020-11-28 21:37:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+"use strick"; // строгий режим
+
+
+
+let flowers = document.querySelector('.flower-1').style.color = "red";
+
+
+
+
+//alert("hello")
+//
+//const result = confirm('it ok?');
+//console.log(result)
+
+
+// const answer = prompt("вам есть 18?", "182222");
+//console.log(answer)
+
+
+//const answers = []
+//
+//
+//answers[0] = prompt('как вас зовут?', '');
+//answers[1] = prompt('как ваша фамилия?', '');
+//answers[2] = prompt('сколько вам лет?', '');
+//
+//document.write(answers)
+
+
+
+let category = 'toys'
+
+console.log(`http://someurl.com/${category}/5/hthth`)
+
+
